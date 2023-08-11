e6f013125771fd26522e3943fd8cdb7fe7929a3e
zer0def <zer0def@zer0def.0>
2023-08-11 08:54:07 +0200

    Tweak for avoiding Magisk-based installs from recovery

--- src/customize.sh
+++ src/customize.sh
@@ -5,5 +5,5 @@ export BOOTMODE MODPATH TMPDIR ZIPFILE ARCH IS64BIT API;
 export MAGISK_VER MAGISK_VER_CODE;
 export KSU KSU_VER KSU_VER_CODE KSU_KERNEL_VER_CODE;
 
-unzip -qoj "$ZIPFILE" "META-INF/com/google/android/update-binary" -d "$TMPDIR/";
-"$TMPDIR/update-binary" "" "" "$ZIPFILE";
+unzip -qoj "${ZIPFILE}" "META-INF/com/google/android/update-binary" -d "${TMPDIR}/MinMicroG/";
+"${TMPDIR}/update-binary" "${1}" "${2}" "${ZIPFILE}" "${@:4}";
